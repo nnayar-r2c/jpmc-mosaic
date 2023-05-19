5645b7e344ee4dca6d159d243eb2831065a06e08
Davie <reid5587@gmail.com>
2023-05-19 13:28:54 +0100

    chore: new way to wait for fonts

--- packages/site/e2e/layout.detailHighlight.test.ts
+++ packages/site/e2e/layout.detailHighlight.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+import { waitForFonts } from './utils';
+
 test('take a screenshot', async ({ page }) => {
   await page.goto('/mosaic/test/layouts/detail-highlight');
-  await page.evaluate(() => document.fonts.ready);
-  await page.waitForFunction(() => document.fonts.check('18px Open Sans'));
+  await page.waitForFunction(waitForFonts);
   await expect(page).toHaveScreenshot('DetailHighlight Layout.png', {
     fullPage: true
   });
